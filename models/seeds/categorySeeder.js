fe4e6ca8f29e8b70cd594eb60c12acd34c2abfdb
subaruuuu <meteorbs13@gmail.com>
2020-11-25 16:03:43 +0800

    feat: add categorySeeder and function

---
+++ models/seeds/categorySeeder.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Record = require('../Record')
+const expense = require('../expense-1.json')
+const spends = expense.results
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+
+  spends.forEach(spend => {
+    Record.create(spend)
+  })
+
+  console.log('done.')
+})
---
+++ models/seeds/recordSeeder.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Record = require('../Record')
+const expense = require('../expense-2.json')
+const spends = expense.results
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+
+
+})
