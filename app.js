a152f77bb1515f5ccf4ce04d0d9d9abd424999af
subaruuuu <meteorbs13@gmail.com>
2020-11-26 01:11:29 +0800

    feat: add sort & totalAmount function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const Record = require('./models/Record')
 
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -15,14 +16,67 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+// handlebars helper
+const helper = exphbs.create({
+  defaultlayout: 'main',
+  helpers: {
+    compare: function (x, y) { return (x === y) }
+  },
+  extname: '.hbs'
+})
+
+// engine 引入 helper
+app.engine('hbs', helper.engine)
 app.set('view engine', 'hbs')
 
 
+// index
 app.get('/', (req, res) => {
-  res.render('index')
+  let totalAmount = 0
+
+  Record.find()
+    .lean()
+    .then(record => {
+      record.forEach(item => {
+        totalAmount += item.amount
+      })
+      res.render('index', { record: record, totalAmount: totalAmount })
+    })
+    .catch(error => console.log(error))
 })
 
+app.get('/sort', (req, res) => {
+  // console.log(req.query)
+  const sort = req.query.options
+  let totalAmount = 0
+
+  if (sort === 'all') {
+    Record.find()
+      .lean()
+      .then(record => {
+        record.forEach(item => {
+          totalAmount += item.amount
+        })
+        res.render('index', { record: record, totalAmount: totalAmount })
+      })
+      .catch(error => console.log(error))
+  } else {
+    Record.find({ category: sort })
+      .lean()
+      .then(record => {
+        record.forEach(item => {
+          totalAmount += item.amount
+        })
+        res.render('index', { record: record, sort: sort, totalAmount: totalAmount })
+      })
+      .catch(error => console.log(error))
+  }
+
+})
+
+
+
+// new
 app.get('/new', (req, res) => {
   res.render('new')
 })
